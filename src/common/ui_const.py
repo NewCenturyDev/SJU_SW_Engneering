4a8fdece120865c0efa1fa8b2cc91c5153facf94
NewCentury99 <smyun99@gmail.com>
2022-12-08 12:51:21 +0900

    Remove useless sources

--- src/common/ui_const.py
+++
@@ -1,9 +0,0 @@
-default_position = {
-    "label_width": 100,
-    "label_height": 25,
-    "entry_width": 200,
-    "entry_height": 25,
-    "title_height_with_padd": 50,
-    "label_width_with_padd": 120,
-    "entry_height_with_padd": 30
-}
